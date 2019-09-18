3aea6abd3ef0124cce907411195492edd776c85a
ola357 <alliolaoluwa@gmail.com>
2019-09-18 01:09:14 +0100

    populate static homepage with data from server

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,39 @@
-let listings;
-$(document).ready(function(){
-    $.get()
-})
+$(document).ready(() => {
+    $.get("http://localhost:3000/listings", (data, status) => {
+        const row1 = document.querySelector('.row1');
+        const row2 = document.querySelector('.row2');
+        for (let index = 0; index < 8; index++) {
+            if (data[index]) {
+                const article = document.createElement('article');
+                const card = document.createElement('div');
+                const img = document.createElement('img');
+                const cardBody = document.createElement('div');
+                const h5 = document.createElement('h5');
+                const p = document.createElement('p');
+                const a = document.createElement('a');
+                if (index < 4) {
+                row1.appendChild(article);
+                } else {
+                row2.appendChild(article);
+                }
+                article.className += 'col-sm-4 ';
+                article.appendChild(card);
+                card.className += 'card ';
+                card.appendChild(img);
+                img.className += 'card-img-top ';
+                img.src = data[index].picUrl
+                card.appendChild(cardBody);
+                cardBody.className += 'card-body ';
+                cardBody.appendChild(h5);
+                h5.className += 'card-title ';
+                h5.innerHTML = `Price: ${data[index].price}`;
+                cardBody.appendChild(p);
+                p.className += 'card-text ';
+                p.innerHTML = `Location: ${data[index].location}`;
+                cardBody.appendChild(a);
+                a.className += 'btn btn-primary ';
+                a.innerHTML += 'View more'
+            }
+        }
+    });
+});
