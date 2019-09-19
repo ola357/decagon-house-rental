3df7aedfcbc34b6b1210d44af501550d1f789d78
ola357 <alliolaoluwa@gmail.com>
2019-09-19 09:01:21 +0100

    login functionality implemented

---
+++ public/js/login.js
@@ -0,0 +1,16 @@
+$('.login').click(()=>{
+    // event.preventDefault();
+    $.get('http://localhost:3000/users', (data, status)=>{
+        const input = document.querySelector('#email').value;
+        for (let index = 0; index < data.length; index++) {
+            const element = data[index];
+            if(element.email === input){
+                alert("yea")
+            } else{
+                alert("nay")
+            }
+            
+        }
+    });
+
+});
